65579a4e232965e3a7f3031e1336f5db837cd962
wangdehuai <wangdehuai@me.com>
2015-04-28 09:38:21 +0800

    Initial Commit

---
+++ UIWilliam/UIWilliam.h
@@ -0,0 +1,19 @@
+//
+//  UIWilliam.h
+//  UIWilliam
+//
+//  Created by wangdehuai on 15/4/28.
+//  Copyright (c) 2015年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIWilliam.
+FOUNDATION_EXPORT double UIWilliamVersionNumber;
+
+//! Project version string for UIWilliam.
+FOUNDATION_EXPORT const unsigned char UIWilliamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWilliam/PublicHeader.h>
+
+
